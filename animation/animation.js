0a296e5bde48efd576a1dbc3b3fe23e646e92d21
tanyong <tanyong@huami.com>
2017-08-01 17:41:51 +0800

    animation pollyfill

--- animation/animation.js
+++ animation/animation.js
@@ -1,18 +1,35 @@
-window.requestAnimationFrame = window.requestAnimationFrame || 
-                                window.mozRequestAnimationFrame ||
-                                window.webkitRequestAnimationFrame ||
-                                window.msRequestAnimationFrame ||
-                                function(callback){
-                                    setTimeout(callback, 1000/60)
-                                }
+window.requestAnimationFrame = (function(){
+     return window.requestAnimationFrame || 
+            window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(callback){
+                setTimeout(callback, 1000/60)
+            }
+}())
 
-
-
-
-
-
-
-
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame) 
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            // 控制在4ms-16ms,时期接近60fps
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    if (!window.cancelAnimationFrame) window.cancelAnimationFrame = function(id) {
+        clearTimeout(id);
+    };
+}());
 
 
 
