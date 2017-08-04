4888cd000b7ef121ab4cce32cbfbcef79eab36e0
tanyong <tanyong@huami.com>
2017-08-04 18:20:56 +0800

    update

--- deferred.js
+++ deferred.js
@@ -1,14 +1,14 @@
 // jquery实现
 
-function Identity( v ) {
+function Identity(v) {
     return v;
 }
 
-function Thrower( ex ) {
+function Thrower(ex) {
     return ex;
 }
 
-function isFunction ( fn ) {
+function isFunction(fn) {
 
 }
 
@@ -18,32 +18,32 @@ function isFunction ( fn ) {
     2、检查是否是有then方法
     3、
 */
-function adoptValue( value, resolve, reject, noValue ) {
+function adoptValue(value, resolve, reject, noValue) {
     var method;
 
     try {
-        if( value && isFunction( ( method = value.promise ) ) ) {
-            method.call( value ).done( resolve ).fail( reject );
-        } else if( value && isFunction( ( method = value.then ) ) ) {
-            method.call( value, resolve, reject );
+        if (value && isFunction((method = value.promise))) {
+            method.call(value).done(resolve).fail(reject);
+        } else if (value && isFunction((method = value.then))) {
+            method.call(value, resolve, reject);
         } else {
-            resolve.apply(undefined, [ value ]).slice( noValue );
+            resolve.apply(undefined, [value]).slice(noValue);
         }
-    } catch( value ) {
+    } catch (value) {
         // call 和 apply 传入null,和 undefined时，需要考虑两种情况
         // 1、全局变量：浏览器window, 其他环境global
         // 2、严格模式下，this就是null或者undefined
-        reject.apply( undefined, [ value ]);
+        reject.apply(undefined, [value]);
     }
 }
 
-function Deferred( func ) {
+function Deferred(func) {
     var tuples = [
             // action, add listener, callbacks,
             // ... .then handlers, argument index, [final state]
-            [ "notify", "progress", jQuery.Callbacks( "memory" ), jQuery.Callbacks( "memory" ), 2 ],
-            [ "resolve", "done", jQuery.Callbacks( "once memory" ), jQuery.Callbacks( "once memory" ), 0, "resolved" ],
-            [ "reject", "fail", jQuery.Callbacks( "once memory" ), jQuery.Callbacks( "once memory" ), 1, "rejected" ]
+            ["notify", "progress", jQuery.Callbacks("memory"), jQuery.Callbacks("memory"), 2],
+            ["resolve", "done", jQuery.Callbacks("once memory"), jQuery.Callbacks("once memory"), 0, "resolved"],
+            ["reject", "fail", jQuery.Callbacks("once memory"), jQuery.Callbacks("once memory"), 1, "rejected"]
         ],
         state = "pending",
         promise = {
@@ -51,23 +51,23 @@ function Deferred( func ) {
                 return state;
             },
             always: function() {
-                deferred.done( arguments ).fail( arguments );
+                deferred.done(arguments).fail(arguments);
                 return this;
             },
             "catch": function() {
-                return promise.then( null, fn );
+                return promise.then(null, fn);
             },
-            pipe: function( /* fnDone, fnFail, fnProgress */) {
+            pipe: function( /* fnDone, fnFail, fnProgress */ ) {
                 var fns = arguments;
 
-                
+
             },
             then: function() {},
             promise: function() {}
         },
         deferred = {};
 
-    each( tuples, function( i, tuple ){
+    each(tuples, function(i, tuple) {
 
     })
 
@@ -75,14 +75,13 @@ function Deferred( func ) {
 }
 
 // Deferred Helper
-function when( singleValue ) {
-    var remaining =arguments.length,
+function when(singleValue) {
+    var remaining = arguments.length,
         i = remaining,
 
-        resolveContexts = Array( i ),
-        resovleValues = slice.call( arguments ),
+        resolveContexts = Array(i),
+        resovleValues = slice.call(arguments),
 
         master = Deferred(),
 
 }
-
