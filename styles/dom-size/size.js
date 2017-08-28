ba7a94177c1ee23bd8ec314f0e7fddc9976d6e2c
tanyong <tanyong@huami.com>
2017-08-28 18:13:46 +0800

    dom size

---
+++ styles/dom-size/size.js
@@ -0,0 +1,14 @@
+    
+    // 1、offsetHeight: 整个元素的高度
+    var p=document.getElementById(p); 
+    var ho=p.offsetHeight; 
+    var hc=p.clientHeight; 
+    console.log(ho,hc); 
+
+    // 2、clientHeight: 整个元素可见区域的高度
+
+
+    // scrollWidth：整个元素可以滚动的高度
+
+    // scrollTop: 获取位于元素顶部边界与元素中当前可见内容的最顶端之间的距离
+
